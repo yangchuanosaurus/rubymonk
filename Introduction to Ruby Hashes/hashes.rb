f1914387b4021b30b934ceb54eba1079d1ef5786
yangchuanosauras <355592261@qq.com>
2018-02-13 20:24:18 +0800

    Learn hashes

---
+++ Introduction to Ruby Hashes/hashes.rb
@@ -0,0 +1,13 @@
+# Createing a Hash
+
+student_ages = {
+	"Jack" => 10, 
+	"Jill" => 12, 
+	"Bob" => 14
+}
+
+puts student_ages["Jack"]
+
+student_ages["Albert"] = 32
+
+puts student_ages
---
+++ Introduction to Ruby Hashes/hashes_in_out.rb
@@ -0,0 +1,55 @@
+# Iterating over a Hash
+student_ages = {
+	"Jack" => 10, 
+	"Jill" => 12, 
+	"Bob" => 14
+}
+
+student_ages.each do |name, age|
+	puts "#{name}'s age is #{age}"
+	age = age + 1 # No effects
+end
+
+puts student_ages
+
+# Ideally, any transformation of a collection should produce 
+# a new collection with the original unchanged making the code easier to understand and manage
+
+student_ages.each do |name, age|
+	student_ages[name] = age + 1 # effects on hashes
+end
+
+puts student_ages
+
+# Extracting the keys and values from a Hash
+puts student_ages.keys
+
+# Newer, faster
+puts student_ages["Albert"].nil?
+
+normal = Hash.new
+p normal
+was_not_there = normal[:zig]
+puts "wasn't there:"
+p was_not_there
+
+usually_brown = Hash.new("brown")
+p usually_brown
+#??????????
+pretending_to_be_there = usually_brown[:zig]
+puts "Pretending to be there"
+p pretending_to_be_there
+puts usually_brown
+
+chuck_norris = Hash[:punch, 99, :kick, 98, :stops_bullets_with_hands, true]
+p chuck_norris
+
+def artax
+	a = [:punch, 0]
+	b = [:kick, 72]
+	c = [:stops_bullets_with_hands, false]
+	key_value_pairs = [a, b, c]
+	Hash[key_value_pairs]
+end
+
+p artax
